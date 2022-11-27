a7b010f9aeeafca7613b1b12a4df7e70b3aa7ae4
Lucas <ldmaia92@hotmail.com>
2022-11-26 21:24:04 -0300

    ajuste na funcao de social networks

--- react-sass/src/components/SocialNetworks.jsx
+++ react-sass/src/components/SocialNetworks.jsx
@@ -3,22 +3,34 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
 import '../styles/components/socialnetworks.sass'
 
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn /> },
-    { name: "github", icon: <FaGithub /> },
-    { name: "instagram", icon: <FaInstagram /> },
-  ];
+  { name: "linkedin", icon: <FaLinkedinIn /> },
+  { name: "github", icon: <FaGithub /> },
+  { name: "instagram", icon: <FaInstagram /> },
+];
 
 const SocialNetworks = () => {
-    return (
-        <section id="social-networks">
-          {socialNetworks.map((network) => (
-            <a href="https://www.linkedin.com/in/matheus-dornelles-02942791/"  className="social-btn" id={network.name} key={network.name}>
-              {network.icon}
-            </a>  
-          ))}
-        
-        </section>
-    );
+  getSocialNetwork(socialNetworks);
 }
+
+function getSocialNetwork(socialNetworks) {
+  socialNetworks.map(network => {
+    if (network.name == "github") {
+      return ((<section id="social-networks">
+
+        <a href="https://github.com/matheusDornelles" className="social-btn" id={network.name} key={network.name}>
+          {network.icon}
+        </a>
+
+      </section>));
+    } else if (network.name == "linkedin") {
+      return (<a href="https://www.linkedin.com/in/matheus-dornelles-02942791/" className="social-btn" id={network.name} key={network.name}>
+        {network.icon}
+      </a>)
+
+    }
+  })
+}
+
+// TODO alterar condicional if else para usar filter do array. mudar function para arrow function
 
 export default SocialNetworks
